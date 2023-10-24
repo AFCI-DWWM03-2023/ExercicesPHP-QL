56e5d0f03d504276288a31411bce747c9f6645e1
noordo406 <leclaire.quentin@free.fr>
2023-10-24 15:36:49 +0200

    exercice 4

--- Exercice 3/8.php
+++ Exercice 3/8.php
@@ -3,9 +3,9 @@
 $nb = (int)readline( "Entrez le nombres de photocopies : " );
 if ($nb < 11) {
     $prix = $nb * 0.1;
-} elseif ($nb < 21) {
+} elseif ($nb < 31) {
     $prix = 1 + ($nb - 10) * 0.09; 
 } else {
-    $prix = 1.9 + ($nb - 20) * 0.08; 
+    $prix = 2.8 + ($nb - 30) * 0.08; 
 }
 echo "Facture : " . $prix . "€";
